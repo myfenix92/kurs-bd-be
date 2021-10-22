ad33f32d9ce4e4ce673d0429a7089195ae4fb1a2
Yana Kornejkova <bloom.hog@gmail.com>
2021-10-22 23:40:06 +0300

    feat: change routes

--- routes/main.routes.js
+++ routes/main.routes.js
@@ -2,9 +2,11 @@ const Routr = require('express');
 const router = new Routr();
 const MainController = require('../controllers/main.controllers');
 
-// 2 post не работают вместе, надо разделять
-router.get('/main', MainController.getAllTables);
+router.get('/main/all_tables', MainController.getAllTables);
+router.get('/main/percent', MainController.getPercentTables);
+router.get('/main/avg', MainController.getAvgCountTables);
 router.get('/main', MainController.filterByParams);
+
 router.post('/main', MainController.createTable);
 
 module.exports = router;
