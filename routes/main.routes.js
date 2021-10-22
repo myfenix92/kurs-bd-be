0f21b55aa5978eceb5bcfd2e4ff7a0453e7a7468
Yana Kornejkova <bloom.hog@gmail.com>
2021-10-22 19:13:30 +0300

    feat: change method routes

--- routes/main.routes.js
+++ routes/main.routes.js
@@ -3,8 +3,8 @@ const router = new Routr();
 const MainController = require('../controllers/main.controllers');
 
 // 2 post не работают вместе, надо разделять
-router.post('/main', MainController.getAllTables);
+router.get('/main', MainController.getAllTables);
 router.get('/main', MainController.filterByParams);
-router.post('/main/create', MainController.createTable);
+router.post('/main', MainController.createTable);
 
 module.exports = router;
