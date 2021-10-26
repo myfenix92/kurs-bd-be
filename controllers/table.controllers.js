9965605fa616cac516eac7918f9a1d7d7d848d89
Yana Kornejkova <bloom.hog@gmail.com>
2021-10-26 18:18:41 +0300

    feat: change query, add error process for name table

--- controllers/table.controllers.js
+++ controllers/table.controllers.js
@@ -5,22 +5,22 @@ class TableController {
   async getTable(req, res) {
     const { id_table }  = req.params;
     const getTableQuery = await db.query(`
-    select kurs.stickers.id_sticker, name_sticker, kurs.stickers.date_create \n
+    select kurs.stickers.id_sticker, name_sticker, kurs.history_changes.date_create \n
     from kurs.user_tables, kurs.stickers \n
     where kurs.user_tables.id_table = kurs.stickers.id_table \n
     and kurs.user_tables.id_table = ($1) \n
-    order by kurs.stickers.date_create desc`, [id_table]);
+    order by kurs.history_changes.date_create desc`, [id_table]);
     res.json(getTableQuery.rows);
   }
 
   async getStickersValue(req, res) {
     const { id_sticker } = req.body;
     const getStickerValueQuery = await db.query(`
-    select kurs.stickers.id_sticker, id_record, record, kurs.records.date_create \n
+    select kurs.stickers.id_sticker, id_record, record, kurs.history_changes.date_create \n
     from kurs.stickers, kurs.records \n
     where kurs.stickers.id_sticker = kurs.records.id_sticker \n
     and kurs.stickers.id_sticker = ($1) \n
-    order by kurs.records.date_create desc`, [id_sticker]);
+    order by kurs.history_changes.date_create desc`, [id_sticker]);
     res.json(getStickerValueQuery.rows)
   }
 
@@ -48,23 +48,30 @@ class TableController {
   async createSticker(req, res) {
     const { id_table, name_sticker } = req.body;
     const createStickerQuery = await db.query(`
-    insert into kurs.stickers (id_table, name_sticker, date_create) values ($1, $2, now())`, [id_table, name_sticker]);
+    insert into kurs.stickers (id_table, name_sticker) values ($1, $2)`, [id_table, name_sticker]);
     res.json(createStickerQuery.rows[0]);
   }
 
   async createRecord(req, res) {
     const { id_sticker, record } = req.body;
     const createRecordQuery = await db.query(`
-    insert into kurs.records (id_sticker, record, date_create) values ($1, $2, now())`, [id_sticker, record]);
+    insert into kurs.records (id_sticker, record) values ($1, $2)`, [id_sticker, record]);
     res.json(createRecordQuery.rows[0]);
   }
   
   async changeNameTable(req, res) {
     const { id_table } = req.params;
     const { new_name_table } = req.body;
-    const changeTableNameQuery = await db.query(`
+    try {
+      const changeTableNameQuery = await db.query(`
       update kurs.user_tables set name_table = ($1) where id_table = ($2) returning *`, [new_name_table, id_table]);
-    res.json(changeTableNameQuery.rows[0]);
+      res.json(changeTableNameQuery.rows[0]);
+    } catch(error) {
+      res.json({
+        code: error.code,
+        message: error.message
+      })
+    }
   }
 
   async changeNameSticker(req, res) {
@@ -86,6 +93,7 @@ class TableController {
     const sortByAlphabetQuery = await db.query(`
     select record from kurs.records, kurs.stickers \n
     where kurs.records.id_sticker = kurs.stickers.id_sticker \n
+    and kurs.records.id_record = kurs.history_changes.id_record \n
     and kurs.stickers.id_sticker = ($1) \n
     order by record`, [id_sticker]);
     res.json(sortByAlphabetQuery.rows);
@@ -94,19 +102,21 @@ class TableController {
   async sortByOld(req, res) {
     const { id_sticker } = req.body
     const sortByAlphabetQuery = await db.query(`
-    select record from kurs.records, kurs.stickers
-    where kurs.records.id_sticker = kurs.stickers.id_sticker
-    and kurs.stickers.id_sticker = ($1)
-    order by kurs.records.date_create`, [id_sticker]);
+    select record from kurs.records, kurs.stickers, kurs.history_changes \n
+    where kurs.records.id_sticker = kurs.stickers.id_sticker \n
+    and kurs.records.id_record = kurs.history_changes.id_record \n
+    and kurs.stickers.id_sticker = ($1) \n
+    order by kurs.history_changes.date_change`, [id_sticker]);
     res.json(sortByAlphabetQuery.rows)
   }
 
   async sortByNew(req, res) {
     const { id_sticker } = req.body
     const sortByAlphabetQuery = await db.query(`
-    select record from kurs.records, kurs.stickers
-    where kurs.records.id_sticker = kurs.stickers.id_sticker
-    and kurs.stickers.id_sticker = ($1)
+    select record from kurs.records, kurs.stickers \n
+    where kurs.records.id_sticker = kurs.stickers.id_sticker \n
+    and kurs.records.id_record = kurs.history_changes.id_record \n
+    and kurs.stickers.id_sticker = ($1) \n
     order by kurs.records.date_create desc`, [id_sticker]);
     res.json(sortByAlphabetQuery.rows)
   }
