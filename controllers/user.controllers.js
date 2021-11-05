1ced879a2da39aec5a0ea7b86a9b7adee382e012
Yana Kornejkova <bloom.hog@gmail.com>
2021-11-05 23:21:00 +0300

    feat: change timestamp im date, add userLogin, add getAboutUser

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -4,33 +4,48 @@ const md5 = require('md5');
 class UserController {
 
     async changeAboutUser(req, res) {
-        const { id_user, password, date_birth, sex } = req.body;
+        const {
+            id_user,
+            password,
+            date_birth,
+            sex
+        } = req.body;
         let addAboutUser;
         let changePassword;
         if (req.body.password !== '' && req.body.date_birth !== '' && req.body.sex !== '') {
             addAboutUser = await db.query(`
-            update kurs.about_users set date_birth = ($1), 
+            update kurs.about_users set date_birth = ($1)::timestamp at time zone 'Europe/Moscow', 
             sex = ($2)
             where id_user = ($3)`, [date_birth, sex, id_user]);
             changePassword = await db.query(`
             update kurs.users set password = ($1)
             where id_user = ($2)`, [password, id_user]);
+        } else
+        if (req.body.password === '' && req.body.date_birth !== '' && req.body.sex !== '') {
+            addAboutUser = await db.query(`
+            update kurs.about_users set date_birth = ($1)::timestamp at time zone 'Europe/Moscow', 
+            sex = ($2)
+            where id_user = ($3)`, [date_birth, sex, id_user]);
         }
-        res.json(addAboutUser.rows[0]);
+        //res.json(addAboutUser.rows[0]);
     }
 
-    async userCreate(req, res){
-        const { login, password, date_birth, sex  } = req.body;
+    async userCreate(req, res) {
+        const {
+            login,
+            password,
+            date_birth,
+            sex
+        } = req.body;
         const loginCheckQuery = await db.query(
             `SELECT count(id_user) FROM kurs.users WHERE login = ($1);`, [login]);
 
-        if(+loginCheckQuery.rows[0].count) {
+        if (+loginCheckQuery.rows[0].count) {
             res.json({
                 regStatus: 0,
                 regText: 'User alredy exist',
             });
-        }
-        else {
+        } else {
             const newUserQuery = await db.query(
                 `INSERT INTO kurs.users VALUES ($1, $2) RETURNING *`, [login, password]);
             res.json({
@@ -40,42 +55,68 @@ class UserController {
             });
             const addAboutUser = await db.query(`
             insert into kurs.about_users (id_user, date_birth, sex, date_registr) 
-            values ($1, ($2), $3, now())`, [newUserQuery.rows[0].id_user, date_birth, sex])
+            values ($1, ($2)::timestamp at time zone 'Europe/Moscow', $3, now())`, [newUserQuery.rows[0].id_user, date_birth, sex])
         }
     }
 
-    async userLogin(req, res){
-        const { login, password } = req.body;
-        const passwordRequest = await db.query(`
-            SELECT id_user, password from kurs.users WHERE login = ($1);`, [login]);
-        const tabPassword = +passwordRequest.rows[0].password;
-        const userExist = password === tabPassword ? true : false;
-        console.log(userExist);
-        if(!userExist){
+    async userLogin(req, res) {
+        const {
+            login,
+            password
+        } = req.body;
+        const loginCheck = await db.query(`
+            SELECT COUNT(login) from kurs.users WHERE login = ($1);`, [login]);
+        if (!(+loginCheck.rows[0].count)) {
             res.json({
-                loginStatus : 0,
-                loginMessage: 'Wrong login or password'
+                loginStatus: 0,
+                loginMessage: 'Wrong login'
             })
+        } else {
+            const passwordRequest = await db.query(`
+                SELECT id_user, password from kurs.users WHERE login = ($1);`, [login]);
+            const tabPassword = +passwordRequest.rows[0].password;
+            const userExist = password === tabPassword ? true : false;
+            if (!userExist) {
+                res.json({
+                    loginStatus: 0,
+                    loginMessage: 'Wrong password'
+                })
+            } else {
+                res.json({
+                    loginStatus: 1,
+                    loginMessage: 'Login success',
+                    loginToken: md5(Date.now()),
+                    id_user: passwordRequest.rows[0].id_user
+                })
+            }
+            const isOnline = await db.query(`
+            UPDATE kurs.users SET online = true WHERE id_user = ($1)`, [passwordRequest.rows[0].id_user]);
         }
-        else{
-            res.json({
-                loginStatus : 1,
-                loginMessage: 'Login success',
-                loginToken: md5(Date.now()),
-                id_user: passwordRequest.rows[0].id_user
-            })
-        }
+
     }
 
-    async userChangePassword(req, res){
-        const { new_password, id_user } = req.body;
+    async getAboutUser(req, res) {
+        const { id_user } = req.params;
+        const aboutUser = await db.query(`
+        select date_birth::timestamp at time zone 'Etc/Greenwich' as date_birth, sex, login from kurs.about_users, kurs.users where kurs.about_users.id_user = ($1) \n
+        and kurs.about_users.id_user = kurs.users.id_user`, [id_user]);
+        res.json(aboutUser.rows[0])
+    }
+
+    async userChangePassword(req, res) {
+        const {
+            new_password,
+            id_user
+        } = req.body;
         const setNewPassQuery = await db.query(
             `UPDATE kurs.users SET password = ($1) WHERE id_user = ($2)`, [new_password, id_user]);
         res.json(setNewPassQuery.rows[0]);
     }
 
-    async deleteUser(req, res){
-        const { id_user } = req.body;
+    async deleteUser(req, res) {
+        const {
+            id_user
+        } = req.body;
         const userDeleteQuery = await db.query(`
             DELETE FROM kurs.users WHERE id_user = ($1)`, [id_user]);
         res.json(userDeleteQuery.rows[0]);
