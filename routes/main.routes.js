9566fd3293cd23e6e35aaa457ab5702e28312834
Yana Kornejkova <bloom.hog@gmail.com>
2021-11-11 01:44:44 +0300

    feat: add middleware

--- routes/main.routes.js
+++ routes/main.routes.js
@@ -1,6 +1,7 @@
 const Routr = require('express');
 const router = new Routr();
 const MainController = require('../controllers/main.controllers');
+const authMiddleware = require('../middlewaree/authMiddleware')
 
 router.get('/main/tables/:id_user', MainController.getAllTables);
 router.get('/main/numeric/:id_user', MainController.getNumericData);
