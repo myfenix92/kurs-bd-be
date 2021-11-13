6ce942f4fc3cabac157f7f9bf6b3d5ee1aeff307
Yana Kornejkova <bloom.hog@gmail.com>
2021-11-13 22:31:30 +0300

    feat: changes routes for changes, history

--- routes/table.routes.js
+++ routes/table.routes.js
@@ -6,17 +6,17 @@ router.get('/table/sticker/:id_sticker', TableController.getStickersValue);
 router.get('/table/sort/alphabet/:id_sticker', TableController.sortByAlphabet);
 router.get('/table/sort/old/:id_sticker', TableController.sortByOld);
 router.get('/table/sort/new/:id_sticker', TableController.sortByNew);
+router.get('/table/history/:id_table', TableController.getHistoryTable);
 router.get('/table/:id_table', TableController.getTable);
 
-router.delete('/table/record/:id_record', TableController.deleteRecord);
 router.delete('/table/sticker/:id_sticker', TableController.deleteSticker);
 router.delete('/table/:id_table', TableController.deleteTable);
 
 router.post('/table/sticker', TableController.createSticker);
 router.post('/table/record', TableController.createRecord);
 
-router.put('/table/record', TableController.changeRecord);
-router.put('/table/sticker', TableController.changeNameSticker);
+router.put('/table/change/record', TableController.changeRecord);
+router.put('/table/change/sticker', TableController.changeNameSticker);
 router.put('/table/:id_table', TableController.changeNameTable);
 router.put('/table/record/done', TableController.isDone);
 
