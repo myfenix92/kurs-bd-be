e91053f7faedad1796228ae3e79b40177f140ad5
Yana Kornejkova <bloom.hog@gmail.com>
2021-11-16 02:46:02 +0300

    feat: add router for get user all days

--- routes/main.routes.js
+++ routes/main.routes.js
@@ -6,6 +6,7 @@ const authMiddleware = require('../middlewaree/authMiddleware')
 router.get('/main/tables/:id_user', MainController.getAllTables);
 router.get('/main/numeric/:id_user', MainController.getNumericData);
 router.get('/main/filter/:id_user', MainController.filterByParams);
+router.get('/main/days/:id_user', MainController.getAllDaysUser);
 
 router.post('/main', MainController.createTable);
 
