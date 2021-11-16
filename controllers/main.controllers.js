47e316d6dfb1bcd47b78fe08aec648473f11e2d7
Yana Kornejkova <bloom.hog@gmail.com>
2021-11-17 01:11:02 +0300

    feat: change query with get date_create table

--- controllers/main.controllers.js
+++ controllers/main.controllers.js
@@ -5,11 +5,14 @@ class MainController {
   async getAllTables(req, res) {
     const { id_user } = req.params;
     const getTableQuery = await db.query(`
-    select id_table, name_table, date_create::timestamp at time zone 'Etc/Greenwich' as date_create, (select kurs.count_record(id_table)) as count_records \n
-    from kurs.user_tables, kurs.users \n
-    where kurs.users.id_user = kurs.user_tables.id_user \n
-    and kurs.users.id_user = ($1) \n
-	  order by id_table desc`, [id_user]);
+    select kurs.user_tables.id_table, name_table, date_change::timestamp at time zone 'Etc/Greenwich' as date_create, \n
+    (select kurs.count_record(kurs.user_tables.id_table)) as count_records \n
+    from kurs.users, kurs.user_tables \n
+    join kurs.history_changes on kurs.history_changes.id_table = kurs.user_tables.id_table \n
+    where kurs.history_changes.changes = 'создали доску' \n
+    and kurs.users.id_user = kurs.user_tables.id_user \n
+    and kurs.users.id_user = ($1)
+    order by id_table desc;`, [id_user]);
     res.json(getTableQuery.rows)
   }
 
@@ -17,7 +20,7 @@ class MainController {
     try {
       const { id_user, nameTable } = req.body;
       const newTable = await db.query(`
-      insert into kurs.user_tables (id_user, name_table, date_create) values ($1, $2, now()::timestamp at time zone 'Europe/Moscow')`, [id_user, nameTable]);
+      insert into kurs.user_tables (id_user, name_table) values ($1, $2)`, [id_user, nameTable]);
       res.json(newTable.rows)
     } catch(error) {
       res.json({
@@ -35,9 +38,12 @@ class MainController {
     //name+ from- to-
     if (nameTable !== '' && dateFrom === '' && dateTo === '') {
       queryString = `
-      select name_table, date_create::timestamp at time zone 'Etc/Greenwich' as date_create, (select kurs.count_record(id_table)) as count_records \n
-      from kurs.user_tables, kurs.users \n
-      where kurs.users.id_user = kurs.user_tables.id_user \n
+      select kurs.user_tables.id_table, name_table, date_change::timestamp at time zone 'Etc/Greenwich' as date_create, \n
+      (select kurs.count_record(kurs.user_tables.id_table)) as count_records \n
+      from kurs.users, kurs.user_tables \n
+      join kurs.history_changes on kurs.history_changes.id_table = kurs.user_tables.id_table \n
+      where kurs.history_changes.changes = 'создали доску' \n
+      and kurs.users.id_user = kurs.user_tables.id_user \n
       and kurs.users.id_user = ($1) \n
       and upper(name_table) like upper($2)
       order by id_table desc;`
@@ -45,72 +51,93 @@ class MainController {
       //name+ from+ to-
     } else if (nameTable !== '' && dateFrom !== '' && dateTo === '') {
       queryString = `
-      select name_table, date_create::timestamp at time zone 'Etc/Greenwich' as date_create, (select kurs.count_record(id_table)) as count_records \n
-      from kurs.user_tables, kurs.users \n
-      where kurs.users.id_user = kurs.user_tables.id_user \n
+      select kurs.user_tables.id_table, name_table, date_change::timestamp at time zone 'Etc/Greenwich' as date_create, \n
+      (select kurs.count_record(kurs.user_tables.id_table)) as count_records \n
+      from kurs.users, kurs.user_tables \n
+      join kurs.history_changes on kurs.history_changes.id_table = kurs.user_tables.id_table \n
+      where kurs.history_changes.changes = 'создали доску' \n
+      and kurs.users.id_user = kurs.user_tables.id_user \n
       and kurs.users.id_user = ($1) \n
       and upper(name_table) like upper($2)
-      and date_create >= ($3)
+      and date_change >= ($3)
       order by id_table desc;`
       getFilterByNameQuery = await db.query(queryString, [id_user, `%${nameTable}%`, dateFrom]);
       //name+ from+ to+
     } else if (nameTable !== '' && dateFrom !== '' && dateTo !== '') {
       queryString = `
-      select name_table, date_create::timestamp at time zone 'Etc/Greenwich' as date_create, (select kurs.count_record(id_table)) as count_records \n
-      from kurs.user_tables, kurs.users \n
-      where kurs.users.id_user = kurs.user_tables.id_user \n
+      select kurs.user_tables.id_table, name_table, date_change::timestamp at time zone 'Etc/Greenwich' as date_create, \n
+      (select kurs.count_record(kurs.user_tables.id_table)) as count_records \n
+      from kurs.users, kurs.user_tables \n
+      join kurs.history_changes on kurs.history_changes.id_table = kurs.user_tables.id_table \n
+      where kurs.history_changes.changes = 'создали доску' \n
+      and kurs.users.id_user = kurs.user_tables.id_user \n
       and kurs.users.id_user = ($1) \n
       and upper(name_table) like upper($2)
-      and (date_create >= ($3) and date_create <= ($4))
+      and (date_change >= ($3) and date_change <= ($4))
       order by id_table desc;`
       getFilterByNameQuery = await db.query(queryString, [id_user, `%${nameTable}%`, dateFrom, dateTo]);
       //name+ from- to+
     } else if (nameTable !== '' && dateFrom === '' && dateTo !== '') {
       queryString = `
-      select name_table, date_create::timestamp at time zone 'Etc/Greenwich' as date_create, (select kurs.count_record(id_table)) as count_records \n
-      from kurs.user_tables, kurs.users \n
-      where kurs.users.id_user = kurs.user_tables.id_user \n
+      select kurs.user_tables.id_table, name_table, date_change::timestamp at time zone 'Etc/Greenwich' as date_create, \n
+      (select kurs.count_record(kurs.user_tables.id_table)) as count_records \n
+      from kurs.users, kurs.user_tables \n
+      join kurs.history_changes on kurs.history_changes.id_table = kurs.user_tables.id_table \n
+      where kurs.history_changes.changes = 'создали доску' \n
+      and kurs.users.id_user = kurs.user_tables.id_user \n
       and kurs.users.id_user = ($1) \n
       and upper(name_table) like upper($2)
-      and date_create <= ($3)
+      and date_change <= ($3)
       order by id_table desc;`
       getFilterByNameQuery = await db.query(queryString, [id_user, `%${nameTable}%`, dateTo]);
       //name- from+ to-
     } else if (nameTable === '' && dateFrom !== '' && dateTo === '') {
       queryString = `
-      select name_table, date_create::timestamp at time zone 'Etc/Greenwich' as date_create, (select kurs.count_record(id_table)) as count_records \n
-      from kurs.user_tables, kurs.users \n
-      where kurs.users.id_user = kurs.user_tables.id_user \n
+      select kurs.user_tables.id_table, name_table, date_change::timestamp at time zone 'Etc/Greenwich' as date_create, \n
+      (select kurs.count_record(kurs.user_tables.id_table)) as count_records \n
+      from kurs.users, kurs.user_tables \n
+      join kurs.history_changes on kurs.history_changes.id_table = kurs.user_tables.id_table \n
+      where kurs.history_changes.changes = 'создали доску' \n
+      and kurs.users.id_user = kurs.user_tables.id_user \n
       and kurs.users.id_user = ($1) \n
-      and date_create >= ($2)
+      and date_change >= ($2)
       order by id_table desc;`
       getFilterByNameQuery = await db.query(queryString, [id_user, dateFrom]);
       //name- from- to+
     } else if (nameTable === '' && dateFrom === '' && dateTo !== '') {
       queryString = `
-      select name_table, date_create::timestamp at time zone 'Etc/Greenwich' as date_create, (select kurs.count_record(id_table)) as count_records \n
-      from kurs.user_tables, kurs.users \n
-      where kurs.users.id_user = kurs.user_tables.id_user \n
+      select kurs.user_tables.id_table, name_table, date_change::timestamp at time zone 'Etc/Greenwich' as date_create, \n
+      (select kurs.count_record(kurs.user_tables.id_table)) as count_records \n
+      from kurs.users, kurs.user_tables \n
+      join kurs.history_changes on kurs.history_changes.id_table = kurs.user_tables.id_table \n
+      where kurs.history_changes.changes = 'создали доску' \n
+      and kurs.users.id_user = kurs.user_tables.id_user \n
       and kurs.users.id_user = ($1) \n
-      and date_create <= ($2)
+      and date_change <= ($2)
       order by id_table desc;`
       getFilterByNameQuery = await db.query(queryString, [id_user, dateTo]);
       //name- from+ to+
     } else if (nameTable === '' && dateFrom !== '' && dateTo !== '') {
       queryString = `
-      select name_table, date_create::timestamp at time zone 'Etc/Greenwich' as date_create, (select kurs.count_record(id_table)) as count_records \n
-      from kurs.user_tables, kurs.users \n
-      where kurs.users.id_user = kurs.user_tables.id_user \n
+      select kurs.user_tables.id_table, name_table, date_change::timestamp at time zone 'Etc/Greenwich' as date_create, \n
+      (select kurs.count_record(kurs.user_tables.id_table)) as count_records \n
+      from kurs.users, kurs.user_tables \n
+      join kurs.history_changes on kurs.history_changes.id_table = kurs.user_tables.id_table \n
+      where kurs.history_changes.changes = 'создали доску' \n
+      and kurs.users.id_user = kurs.user_tables.id_user \n
       and kurs.users.id_user = ($1) \n
-      and (date_create >= ($2) and date_create <= ($3))
+      and (date_change >= ($2) and date_change <= ($3))
       order by id_table desc;`
       getFilterByNameQuery = await db.query(queryString, [id_user, dateFrom, dateTo]);
       //name- from- to-
     } else if (nameTable === '' && dateFrom === '' && dateTo === '') {
       queryString = `
-      select name_table, date_create::timestamp at time zone 'Etc/Greenwich' as date_create, (select kurs.count_record(id_table)) as count_records \n
-      from kurs.user_tables, kurs.users \n
-      where kurs.users.id_user = kurs.user_tables.id_user \n
+      select kurs.user_tables.id_table, name_table, date_change::timestamp at time zone 'Etc/Greenwich' as date_create, \n
+      (select kurs.count_record(kurs.user_tables.id_table)) as count_records \n
+      from kurs.users, kurs.user_tables \n
+      join kurs.history_changes on kurs.history_changes.id_table = kurs.user_tables.id_table \n
+      where kurs.history_changes.changes = 'создали доску' \n
+      and kurs.users.id_user = kurs.user_tables.id_user \n
       and kurs.users.id_user = ($1) \n
       order by id_table desc;`
       getFilterByNameQuery = await db.query(queryString, [id_user]);
@@ -129,14 +156,6 @@ class MainController {
     where kurs.user_tables.id_user = ($1) \n
     and kurs.user_tables.id_user = kurs.users.id_user`, [id_user]);
     res.json(getNumericQuery.rows)
-  }
-
-  async getAllDaysUser(req, res) {
-    const { id_user } = req.params;
-    const getAllDaysQuery = await db.query(`
-    select extract('day' from date_trunc('day',now() - date_registr)) as all_days \n 
-    from kurs.about_users where id_user = ($1);`, [id_user]);
-    res.json(getAllDaysQuery.rows[0]);
   }
 };
 
