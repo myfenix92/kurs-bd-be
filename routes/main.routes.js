ba19d0609bb7189b66a72625cdb810d258ecaffc
Yana Kornejkova <bloom.hog@gmail.com>
2021-11-17 01:10:08 +0300

    feat: delete router, add middleware

--- routes/main.routes.js
+++ routes/main.routes.js
@@ -3,10 +3,9 @@ const router = new Routr();
 const MainController = require('../controllers/main.controllers');
 const authMiddleware = require('../middlewaree/authMiddleware')
 
-router.get('/main/tables/:id_user', MainController.getAllTables);
-router.get('/main/numeric/:id_user', MainController.getNumericData);
+router.get('/main/tables/:id_user', authMiddleware, MainController.getAllTables);
+router.get('/main/numeric/:id_user', authMiddleware, MainController.getNumericData);
 router.get('/main/filter/:id_user', MainController.filterByParams);
-router.get('/main/days/:id_user', MainController.getAllDaysUser);
 
 router.post('/main', MainController.createTable);
 
