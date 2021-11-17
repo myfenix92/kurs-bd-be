a77b4cfc57126a83a4f2feb38629f78caf35b0ee
Yana Kornejkova <bloom.hog@gmail.com>
2021-11-18 01:02:26 +0300

    feat: add middleware

--- routes/table.routes.js
+++ routes/table.routes.js
@@ -3,12 +3,12 @@ const router = new Routr();
 const TableController = require('../controllers/table.controllers');
 const authMiddleware = require('../middlewaree/authMiddleware')
 
-router.get('/table/sticker/:id_sticker', TableController.getStickersValue);
-router.get('/table/sort/alphabet/:id_sticker', TableController.sortByAlphabet);
-router.get('/table/sort/old/:id_sticker', TableController.sortByOld);
-router.get('/table/sort/new/:id_sticker', TableController.sortByNew);
-router.get('/table/history/:id_table', TableController.getHistoryTable);
-router.get('/table/:id_table', TableController.getTable);
+router.get('/table/sticker/:id_sticker', authMiddleware, TableController.getStickersValue);
+router.get('/table/sort/alphabet/:id_sticker', authMiddleware, TableController.sortByAlphabet);
+router.get('/table/sort/old/:id_sticker', authMiddleware, TableController.sortByOld);
+router.get('/table/sort/new/:id_sticker', authMiddleware, TableController.sortByNew);
+router.get('/table/history/:id_table', authMiddleware, TableController.getHistoryTable);
+router.get('/table/:id_table', authMiddleware, TableController.getTable);
 
 router.delete('/table/sticker/:id_sticker', TableController.deleteSticker);
 router.delete('/table/:id_table', TableController.deleteTable);
