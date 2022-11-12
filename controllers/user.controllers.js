a3ff46a45de8ae087424ae4e594ea1e3fa5a367a
Yana Korneykova <bloom.hog@gmail.com>
2022-11-12 18:08:44 +0300

    feat: add dialog, ban, delete

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -109,12 +109,14 @@ class UserController {
 
     async getUsers(req, res) {
         try {
-            const users = await db.query(`select kurs.users.id_user, login, sex, date_birth, date_registr, COALESCE(count(id_table), 0) as count_tables
+            const users = await db.query(`
+            select kurs.users.id_user, login, sex, date_birth, date_registr, COALESCE(count(id_table), 0) as count_tables, ban::timestamp at time zone 'Etc/Greenwich' as ban
             from kurs.about_users, kurs.users
             left join kurs.user_tables on kurs.user_tables.id_user = kurs.users.id_user
             where kurs.users.id_user = kurs.about_users.id_user and kurs.users.id_user <> 1
             group by kurs.users.id_user, login, sex, date_birth, date_registr
-            order by kurs.users.id_user`)
+            order by kurs.users.id_user;`)
+            const checkBan = await db.query(`update kurs.users set ban = case when ban > now() then ban else null end;`);
             res.json(users)
         } catch (e) {
             console.log(e)
@@ -133,22 +135,28 @@ class UserController {
         }
     }
 
-    async getMessagesFromAdmin(req, res) {
-
-    }
-
     async getMessagesFromUser(req, res) {
         try {
-            const msgFromUsers = await db.query(`select id_user, count(id) as count_msg from kurs.support
-            where type_msg = 1 and read_msg = false
-            group by id_user
-            order by id_user`);
+            const msgFromUsers = await db.query(`select kurs.users.id_user, coalesce(count(kurs.support.id_user), 0) as count_msg
+            from kurs.users
+            left join kurs.support on kurs.support.id_user = kurs.users.id_user and type_msg = 1 and read_msg = false
+			group by kurs.users.id_user
+			order by kurs.users.id_user`);
             res.json(msgFromUsers);
         }
 
         catch(e) {
             console.log(e)
         }
+    }
+
+    async getDialog(req, res) {
+        const {
+            id_user
+        } = req.params;
+        const dialogData = await db.query(`select date_sent, message, type_msg from kurs.support where id_user = ($1)
+        order by id;`, [id_user]);
+        res.json(dialogData);
     }
 
     async getAboutUser(req, res) {
@@ -159,6 +167,26 @@ class UserController {
         and kurs.about_users.id_user = kurs.users.id_user \n
         group by date_birth, sex, login, date_registr;`, [id_user]);
         res.json(aboutUser.rows[0])
+    }
+
+    async bannedUser(req, res) {
+        const { time_ban, id_user } = req.body;
+        try {
+        const banUserQuery = await db.query(`update kurs.users set ban = (now() + interval '${time_ban} hour') where id_user = ($1) returning *;`, [id_user]);
+        res.json(banUserQuery.rows[0]);
+    } catch(error) {
+        res.json({
+          code: error.code,
+          message: error.message
+        })
+      }
+      }
+
+    async deleteUser(req, res) {
+        const { id_user } = req.params;
+        const deleteUserQuery = await db.query(`
+        delete from kurs.users where id_user = ($1)`, [id_user]);
+        res.json(deleteUserQuery.rows[0]);
     }
 
     async userChangePassword(req, res) {
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -11,9 +11,13 @@ router.get('/user/:id_user', authMiddleware, userController.getAboutUser);
 
 router.get('/users', roleMiddleware([1]), userController.getUsers);
 router.get('/count_users', roleMiddleware([1]), userController.getCountUsers);
-router.get('/user_msg_from_admin', roleMiddleware([1, 2]), userController.getMessagesFromAdmin); //0 from admin, 1 from user
+//router.get('/user_msg_from_admin', roleMiddleware([1, 2]), userController.getMessagesFromAdmin); //0 from admin, 1 from user
 router.get('/user_msg_from_user', roleMiddleware([1, 2]), userController.getMessagesFromUser); //0 from admin, 1 from user
+router.get('/users/dialog/:id_user', roleMiddleware([1, 2]), userController.getDialog); //0 from admin, 1 from user
 
+router.delete('/users/:id_user', roleMiddleware([1]), userController.deleteUser);
+
+router.put('/ban/:id_user', userController.bannedUser);
 router.put('/profile', userController.changeAboutUser);
 router.put('/logout', userController.userLogout);
 
