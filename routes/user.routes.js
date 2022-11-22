631dd9e3d647794ef76692cb929728d92f37bad0
Yana Korneykova <bloom.hog@gmail.com>
2022-11-23 00:49:24 +0300

    feat: change get router, add check ban, read msg

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -12,13 +12,14 @@ router.get('/user/:id_user', authMiddleware, userController.getAboutUser);
 
 router.get('/users', roleMiddleware([1]), userController.getUsers);
 router.get('/count_users', roleMiddleware([1]), userController.getCountUsers);
-//router.get('/user_msg_from_admin', roleMiddleware([1, 2]), userController.getMessagesFromAdmin); //0 from admin, 1 from user
-router.get('/user_msg_from_user', roleMiddleware([1, 2]), userController.getMessagesFromUser); //0 from admin, 1 from user
+router.get('/user_msg_from_user', roleMiddleware([1, 2]), userController.getCountMessagesFromUser); //0 from admin, 1 from user
 router.get('/users/dialog/:id_user', roleMiddleware([1, 2]), userController.getDialog); //0 from admin, 1 from user
 
 router.delete('/users/:id_user', roleMiddleware([1]), userController.deleteUser);
 
+router.put('/checkban', userController.checkBanTime);
 router.put('/ban/:id_user', userController.bannedUser);
+router.put('/users/dialog/:id_user', userController.readMsg);
 router.put('/profile', userController.changeAboutUser);
 router.put('/logout', userController.userLogout);
 
