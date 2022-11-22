45466e2aa202cd41dc399451a8806b5db79147ae
Yana Korneykova <bloom.hog@gmail.com>
2022-11-23 00:50:46 +0300

    feat: add check ban, read msg, change name method

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -86,6 +86,7 @@ class UserController {
         } else {
             const passwordRequest = await db.query(`
                 SELECT id_user, password, role from kurs.users WHERE login = ($1);`, [login]);
+                
             const validPassword  = bcrypt.compareSync(String(password), passwordRequest.rows[0].password);
             const token = generateAccessToken(passwordRequest.rows[0].id_user, login, passwordRequest.rows[0].role)
             if (!validPassword) {
@@ -98,11 +99,12 @@ class UserController {
                     loginStatus: 1,
                     loginMessage: 'Login success',
                     loginToken: token,
-                    id_user: passwordRequest.rows[0].id_user
+                    id_user: passwordRequest.rows[0].id_user,
                 })
             }
             const isOnline = await db.query(`
-            UPDATE kurs.users SET online = true WHERE id_user = ($1)`, [passwordRequest.rows[0].id_user]);
+        UPDATE kurs.users SET online = true WHERE id_user = ($1)`, [passwordRequest.rows[0].id_user]);
+        res.json(isOnline.rows[0]);
         }
 
     }
@@ -116,7 +118,7 @@ class UserController {
             where kurs.users.id_user = kurs.about_users.id_user and kurs.users.id_user <> 1
             group by kurs.users.id_user, login, sex, date_birth, date_registr
             order by kurs.users.id_user;`)
-            const checkBan = await db.query(`update kurs.users set ban = case when ban > now() then ban else null end;`);
+           
             res.json(users)
         } catch (e) {
             console.log(e)
@@ -135,7 +137,7 @@ class UserController {
         }
     }
 
-    async getMessagesFromUser(req, res) {
+    async getCountMessagesFromUser(req, res) {
         try {
             const msgFromUsers = await db.query(`select kurs.users.id_user, coalesce(count(kurs.support.id_user), 0) as count_msg
             from kurs.users
@@ -155,25 +157,55 @@ class UserController {
             id_user
         } = req.params;
         if (Number(id_user)) {
-            const dialogData = await db.query(`select date_sent::timestamp at time zone 'Etc/Greenwich' as date_sent, message, type_msg from kurs.support where id_user = ($1)
+        
+            const dialogData = await db.query(`
+            select date_sent::timestamp at time zone 'Etc/Greenwich' as date_sent, message, type_msg from kurs.support where id_user = ($1)
             order by id;`, [id_user]);
+
             res.json(dialogData);
         } else {
             const tokenId = jwt.decode(id_user).id_user;
-            const dialogData = await db.query(`select date_sent::timestamp at time zone 'Etc/Greenwich' as date_sent, message, type_msg from kurs.support where id_user = ($1)
+           
+            const dialogData = await db.query(`
+            select date_sent::timestamp at time zone 'Etc/Greenwich' as date_sent, message, type_msg from kurs.support where id_user = ($1)
             order by id;`, [tokenId]);
+ 
             res.json(dialogData);
         }
+
+    }
+
+    async readMsg(req, res) {
+        const {
+            id_user
+        } = req.body;
+        if (Number(id_user)) {
+            const readDialog = await db.query(`
+             update kurs.support set read_msg = true where id_user = ($1) and type_msg = 1;`, [id_user]);
+             
+            res.json(readDialog.rows[0]);
+        } else {
+            const tokenId = jwt.decode(id_user).id_user;
+            const readDialog = await db.query(`
+            update kurs.support set read_msg = true where id_user = ($1) and type_msg = 0;`, [tokenId]);
+            res.json(readDialog.rows[0]);
+        }
+    }
+
+    async checkBanTime(req, res) {
+        const checkBan = await db.query(`update kurs.users set ban = case when ban > now() then ban else null end;`);
+        res.json(checkBan.rows[0]);
 
     }
 
     async getAboutUser(req, res) {
         const { id_user } = req.params;
         const aboutUser = await db.query(`
-        select date_birth::timestamp at time zone 'Etc/Greenwich' as date_birth, sex, login, extract('day' from date_trunc('day',now() - date_registr)) as all_days\n 
-        from kurs.about_users, kurs.users where kurs.about_users.id_user = ($1) \n
-        and kurs.about_users.id_user = kurs.users.id_user \n
-        group by date_birth, sex, login, date_registr;`, [id_user]);
+        select date_birth::timestamp at time zone 'Etc/Greenwich' as date_birth, sex, login, extract('day' from date_trunc('day',now() - date_registr)) as all_days,
+        ban::timestamp at time zone 'Etc/Greenwich' as ban 
+        from kurs.about_users, kurs.users where kurs.about_users.id_user = ($1) 
+        and kurs.about_users.id_user = kurs.users.id_user 
+        group by date_birth, sex, login, date_registr, ban;`, [id_user]);
         res.json(aboutUser.rows[0])
     }
 
@@ -213,6 +245,7 @@ class UserController {
         } = req.body;
         const setOnline = await db.query(
             `UPDATE kurs.users SET online = false WHERE id_user = ($1)`, [id_user]);
+            
         res.json(setOnline.rows[0]);
     }
 
